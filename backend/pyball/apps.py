12d06e794e0ebbc90e781b1f64d3c263c4fe790f
Kapibara-Bba <kapibara.bba0924@gmail.com>
2025-01-19 19:21:45 +0900

    django修正

--- backend/pyball/apps.py
+++ backend/pyball/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppballConfig(AppConfig):
+class PyballConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appball'
+    name = 'pyball'
