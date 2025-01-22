12a630162eefe19b52a27f1a71227f3f2f941796
Kapibara-Bba <kapibara.bba0924@gmail.com>
2025-01-22 23:55:52 +0900

    backendパス設定

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('pyball.urls')),
+    path('api/', include('pyball.urls')),
 ]
