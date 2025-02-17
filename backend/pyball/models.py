719df484241a73087cfd281b783e2b4333d163b9
Kapibara-Bba <kapibara.bba0924@gmail.com>
2025-02-17 12:42:23 +0900

    モデル修正・再生成

--- backend/pyball/models.py
+++ backend/pyball/models.py
@@ -1,46 +1,45 @@
 from django.db import models
 
-class Member(models.Model):
-  team = models.ForeignKey
-  name = models.CharField(max_length=100)
-  password = models.CharField
-  created_date = models.DateTimeField(auto_now_add=True)
-  created_user = models.CharField
-  updated_date = models.DateTimeField(auto_now_add=True)
-  updated_user = models.CharField
 
 class Team(models.Model):
-  team_name = models.CharField
-  director = models.CharField
-  schedule = models.CharField
-  game_result = models.CharField
+  team_name = models.CharField(max_length=100, null=False)
+  director = models.CharField(max_length=100, null=False)
   created_date = models.DateTimeField(auto_now_add=True)
-  created_user = models.CharField
+  created_user = models.CharField(max_length=100, null=True)
   updated_date = models.DateTimeField(auto_now_add=True)
-  updated_user = models.CharField
+  updated_user = models.CharField(max_length=100, null=True)
+
+class Member(models.Model):
+  team = models.ForeignKey(Team, on_delete=models.CASCADE)
+  name = models.CharField(max_length=100, null=False)
+  password = models.CharField(max_length=20, null=False)
+  created_date = models.DateTimeField(auto_now_add=True)
+  created_user = models.CharField(max_length=100, null=True)
+  updated_date = models.DateTimeField(auto_now_add=True)
+  updated_user = models.CharField(max_length=100, null=True)
 
 class Schedule(models.Model):
-  name = models.CharField
-  team = models.ForeignKey
-  plan_date = models.DateField
-  notes = models.CharField
+  schedule_name = models.CharField(max_length=100, null=True)
+  team = models.ForeignKey(Team, on_delete=models.CASCADE)
+  plan_date = models.DateField(null=True)
+  notes = models.CharField(max_length=300, null=True)
   created_date = models.DateTimeField(auto_now_add=True)
-  created_user = models.CharField
+  created_user = models.CharField(max_length=100, null=True)
   updated_date = models.DateTimeField(auto_now_add=True)
-  updated_user = models.CharField
+  updated_user = models.CharField(max_length=100, null=True)
 
 class GameResult(models.Model):
-  team = models.ForeignKey
-  date = models.DateField
-  opponent = models.CharField
-  my_team_score = models.IntegerField
-  opponent_score = models.IntegerField
-  winning_pitcher = models.CharField
-  losing_pitcher = models.CharField
-  save_pitcher = models.CharField
-  home_run = models.CharField
-  notes = models.CharField
+  team = models.ForeignKey(Team, on_delete=models.CASCADE)
+  date = models.DateField(null=True)
+  opponent = models.CharField(max_length=100, null=True)
+  my_team_score = models.IntegerField()
+  opponent_score = models.IntegerField()
+  winning_pitcher = models.CharField(max_length=100, null=True)
+  losing_pitcher = models.CharField(max_length=100, null=True)
+  save_pitcher = models.CharField(max_length=100, null=True)
+  home_run = models.CharField(max_length=100, null=True)
+  notes = models.CharField(max_length=300, null=True)
   created_date = models.DateTimeField(auto_now_add=True)
-  created_user = models.CharField
+  created_user = models.CharField(max_length=100, null=True)
   updated_date = models.DateTimeField(auto_now_add=True)
-  updated_user = models.CharField
+  updated_user = models.CharField(max_length=100, null=True)
